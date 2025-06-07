3cbe6d2591a8dc5dbd7654f9fd79bed5241b304b
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-06-07 22:49:21 +0500

    apply auth & store users

--- 03-kiryana-store-with-auth/app.py
+++ 03-kiryana-store-with-auth/app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, Response, session
 
 app = Flask(__name__)
+app.secret_key = "my_secret"
+
+users_data={}
 
 @app.route("/")
 def home():
@@ -8,12 +11,39 @@ def home():
 
 @app.route("/signup", methods=["GET","POST"])
 def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        age = request.form.get("age")
+        gender = request.form.get("gender")
+
+        users_data[username] = password
+
+        return redirect(url_for('login'))
+    
     return render_template("signup.html")
+
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if username in users_data and users_data[username] == password:
+            session['user'] = username
+            return redirect(url_for("shop"))
+        
+        else:
+            return Response("Invalid credentials",mimetype="text/plain")
+            
+    
     return render_template("login.html")
 
 @app.route("/shop")
 def shop():
     return render_template("shop.html")
+
+@app.route("/users")
+def users():
+    return render_template("users.html", users_data=users_data)
