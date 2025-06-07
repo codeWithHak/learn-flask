2d5ccf9efdd1f66683862a68b34e8c411f73edae
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2025-06-07 22:59:48 +0500

    import tailwind via cdn

--- 03-kiryana-store-with-auth/app.py
+++ 03-kiryana-store-with-auth/app.py
@@ -5,6 +5,14 @@ app.secret_key = "my_secret"
 
 users_data={}
 
+inventory=[
+    {'name':"floor", "price":'120', "available":"200"},
+    {'name':"rice", "price":'220', "available":"100"},
+    {'name':"soap", "price":'90', "available":"300"},
+    {'name':"oil", "price":'500', "available":"250"},
+]
+
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -46,4 +54,4 @@ def shop():
 
 @app.route("/users")
 def users():
-    return render_template("users.html", users_data=users_data)
+    return render_template("users.html", users_data=users_data, inventory=inventory)
